f080c3f62444054dd8e720564542201189ba12ec
crisf <crisf@10.10.80.236>
2019-12-03 21:43:39 -0200

    relatorio

--- src/main/java/br/com/ifgoiano/usuariol/web/ContaBean.java
+++ src/main/java/br/com/ifgoiano/usuariol/web/ContaBean.java
@@ -1,13 +1,20 @@
 package br.com.ifgoiano.usuariol.web;
 
+import java.util.HashMap;
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.model.StreamedContent;
 
 import br.com.ifgoiano.usuariol.conta.Conta;
 import br.com.ifgoiano.usuariol.conta.ContaRN;
+import br.com.ifgoiano.usuariol.util.UtilException;
+import br.com.ifgoiano.usuariol.web.util.RelatorioUtil;
 
 @ManagedBean
 @RequestScoped
@@ -16,6 +23,8 @@ public class ContaBean {
 	private List<Conta> lista = null;
 	@ManagedProperty(value = "#{contextoBean}")
 	private ContextoBean contextoBean;
+	private StreamedContent arquivoRetorno;
+	private int tipoRelatorio;
 
 	public String salvar() {
 		this.selecionada.setUsuario(this.contextoBean.getUsuarioLogado());
@@ -39,6 +48,32 @@ public class ContaBean {
 		contaRN.tornarFavorita(this.selecionada);
 		this.selecionada = new Conta();
 		return null;
+	}
+
+	public StreamedContent getArquivoRetorno() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		String usuario = contextoBean.getUsuarioLogado().getLogin();
+		String nomeRelatorioJasper = "contas"; // (_1_)
+		String nomeRelatorioSaida = usuario + "_contas"; // (_2_)
+		RelatorioUtil relatorioUtil = new RelatorioUtil();
+		HashMap parametrosRelatorio = new HashMap(); // (_3_)
+		parametrosRelatorio.put("codigoUsuario", contextoBean.getUsuarioLogado().getCodigo());
+		try {
+			this.arquivoRetorno = relatorioUtil.geraRelatorio(parametrosRelatorio, nomeRelatorioJasper,
+					nomeRelatorioSaida, this.tipoRelatorio);
+		} catch (UtilException e) {
+			context.addMessage(null, new FacesMessage(e.getMessage()));
+			return null;
+		}
+		return this.arquivoRetorno;
+	}
+
+	public int getTipoRelatorio() {
+		return tipoRelatorio;
+	}
+
+	public void setTipoRelatorio(int tipoRelatorio) {
+		this.tipoRelatorio = tipoRelatorio;
 	}
 
 	public Conta getSelecionada() {
